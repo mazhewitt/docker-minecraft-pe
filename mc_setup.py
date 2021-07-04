01a8af7cc93322abaab6fc13fe2df220cf33e4b7
mazhewitt <mazda@aduki.co.uk>
2021-07-04 14:49:27 +0200

    added user agent to download

--- mc_setup.py
+++ mc_setup.py
@@ -44,7 +44,17 @@ def demote(user_uid, user_gid):
     report_ids('finished demotion')
 
 def grab_download_url(download_page):
-    with urllib.request.urlopen(download_page) as response:
+    req = urllib.request.Request(
+        download_page, 
+        data=None, 
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        }
+    )
+    with urllib.request.urlopen(req) as response:
         html = response.read().decode('utf-8')
         urlMatcher = re.search("(https://minecraft.azureedge.net/bin-linux/[^\"]*)", html)
+        if urlMatcher is None:
+            print("Cannot find the version url - maybe download failed?")
+            print (html)
         return urlMatcher.group()
