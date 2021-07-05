4b499f831514608c56d23c1ff752db0e7206cbf6
mazhewitt <mazda@aduki.co.uk>
2021-07-05 09:11:37 +0200

    updated version grabbing with curl and user agent

---
+++ version.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/bin/curl  -H "Accept-Encoding: identity" -H "Accept-Language: en" -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.$RandNum.212 Safari/537.36" $1
